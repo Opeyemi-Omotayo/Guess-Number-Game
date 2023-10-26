ec85fe218c18f3bf8ddcb66e533545b250cfd940
Opeyemi-Omotayo <opeyemi.motayo1@gmail.com>
2023-10-26 18:52:10 +0200

    bg image

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import StartGameScreen from './screens/StartGameScreen';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, ImageBackground } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
-    <LinearGradient colors={['hotpink', 'deepskyblue']} style={styles.container}>
+    <LinearGradient
+      colors={["hotpink", "deepskyblue"]}
+      style={styles.container}
+    >
       <StatusBar style="auto" />
-      <StartGameScreen />
+
+      <ImageBackground
+        source={require("./assets/images/bg.jpg")}
+        resizeMode="cover"
+        style={styles.container}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15
   }
 });
