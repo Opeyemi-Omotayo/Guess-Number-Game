18386280075a944a73fc90d995a46a3abd1d9ebb
Opeyemi-Omotayo <opeyemi.motayo1@gmail.com>
2023-10-26 17:13:14 +0200

    styling

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,15 +1,30 @@
-import { View, TextInput } from "react-native";
+import { View, TextInput, StyleSheet } from "react-native";
 import Button from "../components/micro/Button";
 
 const StartGameScreen = () => {
   return (
-    <View>
-      <TextInput placeholder="enter a number" />
-      <Button>Reset</Button>
-      <Button>Confirm</Button>
-
-    </View>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="enter a number" />
+        <Button>Reset</Button>
+        <Button>Confirm</Button>
+      </View>
   );
 };
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "hotpink",
+    borderRadius: 8,
+    marginTop: 100,
+    elevation: 8,
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+    marginHorizontal:10,
+  },
+});
